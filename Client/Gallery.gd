4647c110cc4d6cfc5ea94c61fdf9d95332c1ff7a
MasterAle1999 <73252380+MasterAle1999@users.noreply.github.com>
2020-11-11 21:59:06 +0100

    Implementato audio galleria
    -volume regolabile
    -parte con l'apposito bottone

--- Client/Gallery.gd
+++ Client/Gallery.gd
@@ -3,9 +3,11 @@ extends "res://Cards_list_client.gd"
 
 var cards_num = get_card_list_size()-2
 var current_card
+var sound_volume
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	sound_volume=5
 	change_card(1)
 
 func change_card(id):
@@ -20,6 +22,15 @@ func _process(delta):
 		
 	if Input.is_action_just_released("ui_left"):
 		_on_Previous_button_pressed()
+
+remote func play_sound(sound, volume = null):
+	var player = AudioStreamPlayer.new()
+	player.volume_db = sound_volume
+	player.stream = load("res://Resources/" + String(sound) +".wav")
+	self.add_child(player)
+	player.play()
+	yield(player, "finished")
+	self.remove_child(player)
 
 func _on_Previous_button_pressed():
 	if current_card == 1:
@@ -37,3 +48,12 @@ func _on_Next_button_pressed():
 
 func _on_Exit_pressed():
 	get_tree().change_scene("res://Lobby.tscn")
+
+
+
+func _on_HSlider_value_changed(value):
+	sound_volume = linear2db(value)
+
+
+func _on_Button_pressed():
+	play_sound(current_card)
